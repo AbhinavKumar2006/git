2e4f3f64df6ec843f3839ea1c5509c4b832537e3
Abhinav Kumar <annupk2006@gmail.com>
2021-03-26 18:55:46 +0530

    class

--- sketch.js
+++ sketch.js
@@ -3,8 +3,9 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1 , pig3;
 var backgroundImg,platform;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    pig3 = new Pig(400,200)
 
 }
 
